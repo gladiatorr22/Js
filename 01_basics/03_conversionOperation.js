218bbfc24ebc22efe297022ea3464e6ec218cecf
gladiatorr22 <likhitnataraj@gmail.com>
2025-09-13 10:05:32 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// let score = "33abc"
+// console.log(typeof score) //number
+// let valueInNumber = Number(score) //convert to number
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //NaN
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn) //convert to boolean
+// console.log(booleanIsLoggedIn) //true
+
+let someNumber = 33
+let stringNumber = String(someNumber) //convert to string
+console.log(typeof stringNumber) //string
+console.log(stringNumber) //33
