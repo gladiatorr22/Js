be1d848916a2368fda7b89c868892e408f79ea8a
gladiatorr22 <likhitnataraj@gmail.com>
2025-09-13 20:48:30 +0530

    updated basics_2

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -44,13 +44,13 @@
 // }
 // console.log(regularuser.userfullname.fullname.lastname)
 
-// const obj1 ={1:'a',2:'b'}
-// const obj2 ={3:'a',4:'b'}
+const obj1 ={1:'a',2:'b'}
+const obj2 ={3:'a',4:'b'}
 
-// const obj3={obj1,obj2}
-// const obj3 = Object.assign({},obj1,obj2)
-// const obj4 = {...obj1,...obj2}
+//const obj3={obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2)
+const obj4 = {...obj1,...obj2}
 
-// console.log(obj4)
+console.log(obj4)
 
 
