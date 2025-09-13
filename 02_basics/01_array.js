c25ee2904fc6e1a8f49aa53e3710da9b7aa05369
gladiatorr22 <likhitnataraj@gmail.com>
2025-09-13 20:47:47 +0530

    basics_2

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+// let arr = new Array(1,2,3,4,5)
+// console.log(arr);
+
+//  arr.push(9)
+// console.log(arr);
+// arr.unshift(8)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// const newarr = arr.join()
+// console.log(newarr)
+
+// const slice1=arr.slice(0,3)
+// console.log(slice1)
+
+// console.log(arr.splice(2,0));
+
+
+arr3=[1,2,[3,4],5,[5,3]]
+const arr4=arr3.flat(Infinity)
+console.log(arr4)
---
+++ 02_basics/02_objects.js
@@ -0,0 +1,56 @@
+// //object literals
+// const mysym=Symbol("key1")
+// const jsuser ={
+//     name:"likith",
+//     [mysym] : "key1",
+//     age:22,
+//     loc:"blr",
+//     isloggedin:false
+
+// }
+
+// // console.log(jsuser["age"])
+// // console.log(jsuser[mysym])
+// // console.log(typeof(mysym))
+
+// jsuser.greeting =  function() {
+//     console.log(`hello user ${this.name}`);  
+// }
+
+// console.log(jsuser.greeting())
+
+
+// ***********************************************************************************************88
+
+// singleton => const tinder = new Object()
+// non-singleton => const tinder ={}
+
+// const tinderuser={}
+// tinderuser.name = "likith"
+// tinderuser.age=22
+// tinderuser.isactive=true
+// console.log(Object.values(tinderuser))
+// console.log(Object.keys(tinderuser))
+// console.log(tinderuser)
+
+// const regularuser={
+//     email:"some@gmail.com",
+//     userfullname:{
+//         fullname:{
+//             firstname:'likith',
+//             lastname:'raj'
+//         }
+//     }
+// }
+// console.log(regularuser.userfullname.fullname.lastname)
+
+// const obj1 ={1:'a',2:'b'}
+// const obj2 ={3:'a',4:'b'}
+
+// const obj3={obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+// const obj4 = {...obj1,...obj2}
+
+// console.log(obj4)
+
+
