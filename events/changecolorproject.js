7c9aa26c008c84ce37f16d1345b9847b942aed9c
gladiatorr22 <likhitnataraj@gmail.com>
2025-09-20 20:47:27 +0530

    event-project

---
+++ events/changecolorproject.js
@@ -0,0 +1,24 @@
+const randomcolor = function(){
+  const hex = '0123456789ABCDEF'
+  let color = '#'
+  for (let i=0 ; i<6 ; i++){
+    color += hex[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+let stop
+const startchangingcolor = function(){
+  stop = setInterval(changebodycolor,1000)
+  function changebodycolor(){ 
+  document.querySelector('body').style.backgroundColor = randomcolor()}
+}
+
+const stopchaningcolor = function(){
+  clearInterval(stop)
+  stop=null
+}
+
+document.querySelector('#stop').addEventListener('click',stopchaningcolor)
+document.querySelector('#start').addEventListener('click',startchangingcolor)
+
